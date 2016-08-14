06476b46758ca8f1b2e641a3f78bdcc3165040d3
samer al-khatib <samer_mail_2006@yahoo.com>
2016-08-14 23:21:59 +0300

    Add  ReactJs Example.

---
+++ ReactJs/Controllers/ReactJsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReactJs.Controllers
+{
+    public class ReactJsController : Controller
+    {
+        // GET: ReactJs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Component()
+        {
+            return View();
+        }
+
+        public ActionResult Props()
+        {
+            return View();
+        }
+
+        public ActionResult State()
+        {
+            return View();
+        }
+        
+    }
+}
